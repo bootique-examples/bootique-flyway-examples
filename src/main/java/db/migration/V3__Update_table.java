3969f3f618940d0449bc62b993014015e9855024
Elena Bondareva <ebondareva@objectstyle.com>
2017-07-05 14:52:21 +0300

    provide try-with-resources

--- src/main/java/db/migration/V3__Update_table.java
+++ src/main/java/db/migration/V3__Update_table.java
@@ -1,8 +1,6 @@
 package db.migration;
 
 import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -11,17 +9,14 @@ import java.sql.PreparedStatement;
  * Example of a Java-based migration.
  */
 public class V3__Update_table implements JdbcMigration {
-    private static Logger LOGGER = LoggerFactory.getLogger(V3__Update_table.class);
 
     @Override
     public void migrate(Connection connection) throws Exception {
-        PreparedStatement statement =
-                connection.prepareStatement("INSERT INTO TEST (name) VALUES ('test3')");
 
-        try {
+        try (PreparedStatement statement =
+                     connection.prepareStatement("INSERT INTO TEST (name) VALUES ('test3')")) {
+
             statement.execute();
-        } finally {
-            statement.close();
         }
     }
 }
