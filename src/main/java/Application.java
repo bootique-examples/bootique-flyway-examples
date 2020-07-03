9a89912fa238fceaf02071721710aa399566b066
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2020-07-03 10:51:10 +0200

    Based on Bootique version 2.0 and Flyway 6.5.0.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,19 +1,11 @@
-import com.google.inject.Binder;
-import com.google.inject.Module;
 import io.bootique.Bootique;
 
-public class Application implements Module {
+public class Application {
 
     public static void main(String[] args) {
         Bootique.app(args)
                 .autoLoadModules()
-                .module(Application.class)
                 .exec()
                 .exit();
-    }
-
-    @Override
-    public void configure(Binder binder) {
-
     }
 }
