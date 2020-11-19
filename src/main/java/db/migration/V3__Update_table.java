f945c9b4f6e6f804ed2fd016891e767388cbc0a6
andreykravchenko <andrey.kravchenko@objectstyle.com>
2020-11-19 17:00:15 +0300

    Update version to 2.0

--- src/main/java/db/migration/V3__Update_table.java
+++ src/main/java/db/migration/V3__Update_table.java
@@ -2,22 +2,31 @@ package db.migration;
 
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * Example of a Java-based migration.
  */
 public class V3__Update_table extends BaseJavaMigration {
+    private static Logger LOGGER = LoggerFactory.getLogger(V3__Update_table.class);
 
     @Override
     public void migrate(Context context) throws Exception {
-        try (Connection connection = context.getConnection()){
-            try(PreparedStatement statement = connection
-                    .prepareStatement("INSERT INTO TEST (name) VALUES ('test3')")) {
-                statement.execute();
-            }
+        Connection connection = context.getConnection();
+        PreparedStatement statement =
+                connection.prepareStatement("INSERT INTO TEST (name) VALUES ('test3')");
+
+        try {
+            statement.execute();
+        } catch (SQLException e) {
+            LOGGER.error("Migration failed", e);
+        } finally {
+            statement.close();
         }
     }
 }
